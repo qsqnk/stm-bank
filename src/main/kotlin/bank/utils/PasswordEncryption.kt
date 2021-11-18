41fabc0403a55f659f033ebadbb78390bcc9e3f8
Alexander Fedkin <st087201@student.spbu.ru>
2021-11-18 23:31:46 +0300

    add password encrypt

--- src/main/kotlin/bank/utils/PasswordEncryption.kt
+++ src/main/kotlin/bank/utils/PasswordEncryption.kt
@@ -1,2 +1,11 @@
 package bank.utils
 
+import bank.api.Password
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
+fun Password.encrypted(): String {
+    val spec = PBEKeySpec(toCharArray())
+    val encAlgo = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
+    return encAlgo.generateSecret(spec).encoded.toString()
+}
